e91811e70ed3fde71de3b66a1db22bead36b131d
Luke Gormley <luke.gormley@bath.edu>
2022-06-08 14:57:04 +0100

    amended file for optimizing speech recognition with key words

--- wots_cookin/google_api.py
+++ wots_cookin/google_api.py
@@ -1,24 +1,32 @@
 from google.cloud import speech_v1 as speech
+import pandas as pd
 
 def speech_to_text(config, audio):
     """function to convert audio to text and execute print_sentences function"""
     client = speech.SpeechClient()
     response = client.recognize(config=config, audio=audio)
     for result in response.results:
-        # The first alternative is the most likely one for this portion.
+        # The transcript of first alternative is returned
         return result.alternatives[0].transcript
 
+#downloading list of ingredients with count and splitting by popularity for model optimizing
+df = pd.read_csv('data/ingredients_list.csv')
+top_list = list(df.iloc[0:1000, 1])
+mid_list = list(df.iloc[1000:2000, 1])
+bottom_list = list(df.iloc[2000:, 1])
+
+#speech configeration settings, boosted for ingredient words
 config = speech.RecognitionConfig(language_code = "en-GB",
             sample_rate_hertz = 48000,
             model = 'default',
             speech_contexts = [{
-                "phrases": ["list to be added with most popular ingredients"],
+                "phrases": top_list,
                 "boost": 10
                 }, {
-                "phrases": ["list to be added with mid popular ingredients"],
+                "phrases": mid_list,
                 "boost": 6
                 }, {
-                "phrases": ["list to be added with lowest popular ingredients"],
+                "phrases": bottom_list,
                 "boost": 3
                 }]
             )
