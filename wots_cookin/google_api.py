cb534b3a1198a5c22f973eb86d73a3b67aac2db1
Luke Gormley <luke.gormley@bath.edu>
2022-06-08 13:44:54 +0100

    added model boosting with ingredient words

--- wots_cookin/google_api.py
+++ wots_cookin/google_api.py
@@ -8,9 +8,17 @@ def speech_to_text(config, audio):
         # The first alternative is the most likely one for this portion.
         return result.alternatives[0].transcript
 
-config = speech.RecognitionConfig(language_code="en-GB",
-#             sample_rate_hertz = 48000,
-#             enable_automatic_punctuation=True,
-#             adaptation=speech_adaptation,
-#             model = 'default'
+config = speech.RecognitionConfig(language_code = "en-GB",
+            sample_rate_hertz = 48000,
+            model = 'default',
+            speech_contexts = [{
+                "phrases": ["list to be added with most popular ingredients"],
+                "boost": 10
+                }, {
+                "phrases": ["list to be added with mid popular ingredients"],
+                "boost": 6
+                }, {
+                "phrases": ["list to be added with lowest popular ingredients"],
+                "boost": 3
+                }]
             )
