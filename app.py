7329a3484786b3c947fb8b1c48c24062a066c616
Den Chan <98968275+denwychan@users.noreply.github.com>
2022-06-15 11:26:18 +0100

    Opimised the code to make loading speed quicker

--- app.py
+++ app.py
@@ -12,54 +12,6 @@ from wots_cookin.word2vec_trainer import Trainer
 from wots_cookin.shortlist import *
 from wots_cookin.display import print_details
 
-st.markdown("""# Wots Cookin
-## Hey there! What's in your fridge?""")
-
-#audio record button
-record_button  = Button(label="Record", width=100)
-
-#java script to run audio recording
-
-record_button.js_on_event("button_click", CustomJS(code="""
-//Get button from the DOM
-var button = document.getElementsByTagName('button')[0];
-
-if (button.textContent == "Record") {
-    const timeMilliSec = 10000 //Fixed 10sec recording ... change here the value
-    navigator.mediaDevices.getUserMedia({ audio: true })
-    .then(stream => {
-        window.mediaRecorder = new MediaRecorder(stream);
-        window.mediaRecorder.start();
-        const audioChunks = [];
-        window.mediaRecorder.addEventListener("dataavailable", event => {
-            audioChunks.push(event.data);
-        });
-        window.mediaRecorder.addEventListener("stop", () => {
-            //convert audioBuffer to wav
-            const audioBlob = new Blob(audioChunks, {type:'audio/wav'});
-            //create base64 reader
-            var reader = new FileReader();
-            reader.readAsDataURL(audioBlob);
-            reader.onloadend = function() {
-                //read base64
-                var base64data = reader.result;
-                //send data to streamlit
-                document.dispatchEvent(new CustomEvent("GET_AUDIO_BASE64", {detail: base64data}));
-            }
-        });
-        //Automatic stop after a set timeframe
-        //setTimeout(() => {
-        //window.mediaRecorder.stop();
-        //}, timeMilliSec);
-
-    });
-    button.textContent = "Stop";
-} else {
-    mediaRecorder.stop();
-    button.textContent = "Record";
-}
-"""))
-
 # Load clean dataframe of recipes
 df = pd.read_pickle("raw_data/enriched_recipes.pkl")
 df.drop(columns = ['index'], inplace = True)
@@ -76,93 +28,144 @@ print(model)
 print(model.wv.__dict__.keys())
 print(model.wv.__dict__['index_to_key'][:10])
 
-#code to extract audio results from JS to python
-result = streamlit_bokeh_events(
-    record_button,
-    events="GET_AUDIO_BASE64",
-    key="listen",
-    refresh_on_update=False,
-    override_height=75,
-    debounce_time=0)
+def main():
+    st.markdown("""## Hey there! What's in your fridge?""")
 
-#dietary side-bar checklist
-dietary_requirements = st.sidebar.multiselect(
-    'What are your dietary requirements?',
-    ['Vegetarian', 'Vegan', 'Gluten free', 'Nut free','No shellfish',
-     'No eggs', 'Dairy free', 'No soy' ]
-     )
+    #audio record button
+    record_button  = Button(label="Record", width=100)
 
-#minimum number of ingredients selection
-min_num_ingredients = st.sidebar.selectbox(
-    'Minimum number of ingredients', [1,2,3,4,5,6,7,8,9,10])
+    #java script to run audio recording
 
-if result:
-    if "GET_AUDIO_BASE64" in result:
-        st.write("Audio recording completed")
-        b64_str_metadata = result.get("GET_AUDIO_BASE64")
-        metadata_string = "data:audio/wav;base64,"
-        if len(b64_str_metadata)>len(metadata_string):
-            #get rid of metadata (data:audio/wav;base64,)
+    record_button.js_on_event("button_click", CustomJS(code="""
+    //Get button from the DOM
+    var button = document.getElementsByTagName('button')[0];
 
-            if b64_str_metadata.startswith(metadata_string):
-                b64_str = b64_str_metadata[len(metadata_string):]
-            else:
-                b64_str = b64_str_metadata
+    if (button.textContent == "Record") {
+        const timeMilliSec = 10000 //Fixed 10sec recording ... change here the value
+        navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+            window.mediaRecorder = new MediaRecorder(stream);
+            window.mediaRecorder.start();
+            const audioChunks = [];
+            window.mediaRecorder.addEventListener("dataavailable", event => {
+                audioChunks.push(event.data);
+            });
+            window.mediaRecorder.addEventListener("stop", () => {
+                //convert audioBuffer to wav
+                const audioBlob = new Blob(audioChunks, {type:'audio/wav'});
+                //create base64 reader
+                var reader = new FileReader();
+                reader.readAsDataURL(audioBlob);
+                reader.onloadend = function() {
+                    //read base64
+                    var base64data = reader.result;
+                    //send data to streamlit
+                    document.dispatchEvent(new CustomEvent("GET_AUDIO_BASE64", {detail: base64data}));
+                }
+            });
+            //Automatic stop after a set timeframe
+            //setTimeout(() => {
+            //window.mediaRecorder.stop();
+            //}, timeMilliSec);
 
-            decoded = base64.b64decode(b64_str)
+        });
+        button.textContent = "Stop";
+    } else {
+        mediaRecorder.stop();
+        button.textContent = "Record";
+    }
+    """))
 
-            #save it server side
-            uploaded_file = 'audio_files/test.flac'
-            with open(uploaded_file,'wb') as f:
-                f.write(decoded)
+    #code to extract audio results from JS to python
+    result = streamlit_bokeh_events(
+        record_button,
+        events="GET_AUDIO_BASE64",
+        key="listen",
+        refresh_on_update=False,
+        override_height=75,
+        debounce_time=0)
 
-            #convert File to flac and save it again
-            flac = AudioSegment.from_file(uploaded_file)
-            getaudio = flac.export(uploaded_file, format="flac")
+    #dietary side-bar checklist
+    dietary_requirements = st.sidebar.multiselect(
+        'What are your dietary requirements?',
+        ['Vegetarian', 'Vegan', 'Gluten free', 'Nut free','No shellfish',
+        'No eggs', 'Dairy free', 'No soy' ]
+        )
 
-            #convert audio to text with google API
-            with open(uploaded_file, "rb") as audio_file:
-                content = audio_file.read()
-            audio = speech.RecognitionAudio(content=content)
-            transcript = speech_to_text(config, audio)
+    #minimum number of ingredients selection
+    min_num_ingredients = st.sidebar.selectbox(
+        'Minimum number of ingredients', [1,2,3,4,5,6,7,8,9,10])
 
-            st.write(f'Recording: {transcript}')
+    if result:
+        if "GET_AUDIO_BASE64" in result:
+            st.write("Audio recording completed")
+            b64_str_metadata = result.get("GET_AUDIO_BASE64")
+            metadata_string = "data:audio/wav;base64,"
+            if len(b64_str_metadata)>len(metadata_string):
+                #get rid of metadata (data:audio/wav;base64,)
 
-            #Filter dietary requirements
-            shortlist_df = filter_diet_req(dietary_requirements, df)
-            print(df.head(5))
+                if b64_str_metadata.startswith(metadata_string):
+                    b64_str = b64_str_metadata[len(metadata_string):]
+                else:
+                    b64_str = b64_str_metadata
 
-            #filter recipe list for minimum number of ingredients
-            shortlist_df = filter_min_ingredients(min_num_ingredients, shortlist_df)
-            print(df.head(5))
+                decoded = base64.b64decode(b64_str)
 
-            # Preprocess transcript into ingredients list
-            ingredients = transcript.split()
-            ingredients = remove_stopwords_from_list(ingredients, stopwords)
-            print(ingredients)
+                #save it server side
+                uploaded_file = 'audio_files/test.flac'
+                with open(uploaded_file,'wb') as f:
+                    f.write(decoded)
 
-            # Vectorise the ingredients list form the transcript
-            ing_vector = get_ingredients_vector(model, ingredients)
-            print(ing_vector)
+                #convert File to flac and save it again
+                flac = AudioSegment.from_file(uploaded_file)
+                getaudio = flac.export(uploaded_file, format="flac")
 
-            # Get shortlist recipes from model
-            shortlist_df = get_similar_recipes(ing_vector, shortlist_df['Vector_List'], shortlist_df)
-            print(shortlist_df.shape)
+                #convert audio to text with google API
+                with open(uploaded_file, "rb") as audio_file:
+                    content = audio_file.read()
+                audio = speech.RecognitionAudio(content=content)
+                transcript = speech_to_text(config, audio)
 
-            # Create slider to allow the user to select the number of results
-            # in the results dataframe
-            results_count = st.slider('Select number of results', 1, 10, 5)
+                st.write(f'Recording: {transcript}')
 
-            #using search function to find no.1 matching recipe
-            top_recipes_df = shortlist_recipes(shortlist_df, ingredients, shortlist_len=results_count)
-            print(top_recipes_df.shape)
+                #Filter dietary requirements
+                shortlist_df = filter_diet_req(dietary_requirements, df)
+                print(df.head(5))
 
-            # and used in order to select the displayed lines
-            st.dataframe(top_recipes_df[['Title'
-                                         , 'Match_Score'
-                                         , 'Ingredients_Available']], 900)
+                #filter recipe list for minimum number of ingredients
+                shortlist_df = filter_min_ingredients(min_num_ingredients, shortlist_df)
+                print(df.head(5))
 
-            #print list of recipes including ingredients (flagging missing ingredients)
-            #and instructions
-            st.title('Recipe Details:')
-            print_details(top_recipes_df, ingredients)
+                # Preprocess transcript into ingredients list
+                ingredients = transcript.split()
+                ingredients = remove_stopwords_from_list(ingredients, stopwords)
+                print(ingredients)
+
+                # Vectorise the ingredients list form the transcript
+                ing_vector = get_ingredients_vector(model, ingredients)
+                print(ing_vector)
+
+                # Get shortlist recipes from model
+                shortlist_df = get_similar_recipes(ing_vector, shortlist_df['Vector_List'], shortlist_df)
+                print(shortlist_df.shape)
+
+                # Create slider to allow the user to select the number of results
+                # in the results dataframe
+                results_count = st.slider('Select number of results', 1, 10, 5)
+
+                #using search function to find no.1 matching recipe
+                top_recipes_df = shortlist_recipes(shortlist_df, ingredients, shortlist_len=results_count)
+                print(top_recipes_df.shape)
+
+                # and used in order to select the displayed lines
+                st.dataframe(top_recipes_df[['Title'
+                                            , 'Match_Score'
+                                            , 'Ingredients_Available']], 900)
+
+                #print list of recipes including ingredients (flagging missing ingredients)
+                #and instructions
+                st.title('Recipe Details:')
+                print_details(top_recipes_df, ingredients)
+
+if __name__ == '__main__':
+    main()
